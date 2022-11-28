02ca73d883f076d3017ad071fb4f9d1b506e29d3
7 <vadim.vidineev1@.gmail.com>
2022-11-28 18:16:39 +0200

    Version 1.2

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,2 +1,17 @@
-print("MAIN")
-lst = []
+class Student:
+    education = 'STER'
+    group = 1920
+
+    def __init__(self, name):
+        self.name = 'vadim'
+        self.age = '12'
+        self.height = '156'
+        print('Создался обьект')
+
+
+
+
+
+
+st1 = Student(name='Anna', age=17,height=170)
+st1 = Student(name='Igor', age=16,height=180)
