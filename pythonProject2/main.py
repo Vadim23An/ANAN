3eddb903dd4c1956423dcfe4fabce3492381686f
7 <vadim.vidineev1@.gmail.com>
2022-12-09 16:40:46 +0200

    proect1

---
+++ pythonProject2/main.py
@@ -0,0 +1,17 @@
+class Human:
+    def __init__(self, name='Human'):
+        self.name = name
+
+class Auto
+    def __init__(self, drand):
+        self.drand = drand
+        self.passengers = []
+
+        def add_passengers(self, humen):
+            self.passengers.append(humen)
+
+        def add_passengers
+
+
+        Kate = Human('Kate')
+        nick = humen
